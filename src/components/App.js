deee5770da89b21fe699e68c52c40b4b0f3e20d2
vishva Shah <vishu885>
2023-08-30 15:08:56 -0400

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
-
+ import React from "react";
+import {data} from "../data";
+import Navbar from "./Navbar";
+import Moviecard from "./Moviecard";
 function App() {
+
   return (
     <div className="App">
-      project Setup
+      
+      <Navbar/>
+
+      <div className="main">
+        <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+      </div>
+
+      <div className="list">
+          {data.map((movie,index)=>(
+              <Moviecard movie={movie} />
+          ))}
+
+        </div>
+      </div>
     </div>
   );
 }
